238fd6f06685ae2c11230669e405d613ccd04cbd
PDemenkov <guapnik@gmail.com>
2022-09-07 20:15:19 +0300

    Задание 4

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Задание 4");
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend *2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+    }
+}
